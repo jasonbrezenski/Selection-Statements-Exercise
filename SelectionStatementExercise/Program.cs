16dd0a5e1811fc7fe107eb120e8e29684f165f03
Jason Brezenski <jasonbrezenski@@yahoo.com>
2024-08-13 13:27:29 -0500

    Selection Statements Exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            GuessTheNumber();
+        }
+
+        public static int GuessTheNumber()
+        {
+            Random r = new Random();
+            int favNumber = r.Next(1, 21);
+            
+            Console.WriteLine("Guess my favorite number between 1 & 20:");
+            int userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber) 
+            {
+                Console.WriteLine("Too low");
+            }
+            
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Too high");
+            }
+            
+            else
+            {
+                Console.Write("You guessed correctly!!!");
+            }
+
+            return userInput;
         }
     }
 }
